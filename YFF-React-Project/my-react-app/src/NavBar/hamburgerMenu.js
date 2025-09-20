57e8c75bc957a4391d661401a32418327e8c4c4e
KnutMQS <knutmqs@gmail.com>
2025-09-20 16:46:45 +0200

    nav bar layout and hamburger menu animation

--- YFF-React-Project/my-react-app/src/NavBar/hamburgerMenu.js
+++ YFF-React-Project/my-react-app/src/NavBar/hamburgerMenu.js
@@ -1,27 +1,26 @@
 import { useState } from "react";
-import { FaBars, FaTimes, FaUser, FaSignInAlt } from "react-icons/fa";
+import { HiBars3, HiXMark } from "react-icons/hi2";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { RiMenuLine, RiCloseLine } from "react-icons/ri";
 import "./navBar.css";
 
 function HamburgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="hamburger-menu">
+    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
       <button
         className="hamburger-icon"
         onClick={() => setIsOpen((open) => !open)}
         aria-label="Toggle menu"
       >
-        {isOpen ? <FaTimes /> : <FaBars />}
+        {isOpen ? <HiXMark size={42} /> : <HiBars3 size={42} />}
       </button>
-      {isOpen && (
         <div className="menu-items">
           <button className="login-button">
             Login
-            <FaUser />
           </button>
           <button className="signup-button">
-            <FaSignInAlt />
             Sign Up
           </button>
           <div className="navbar-center">
@@ -38,7 +37,6 @@ function HamburgerMenu() {
             </ul>
           </div>
         </div>
-      )}
     </div>
   );
 }
